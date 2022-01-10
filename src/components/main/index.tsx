c52ba3779a56398082b48602ff6d4858333fdd59
Lucas Rosa <lucasmetron@gmail.com>
2022-01-10 10:56:54 -0300

    styled component configurado com next

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import * as S from './styles' //boa pratica para sabermos que estamos utilizando um estilo
 
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="imagem de um àtomo e react avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJs, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor em frente a uma tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/main/test.tsx
+++ src/components/main/test.tsx
@@ -15,4 +15,10 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' }) //busca se possui essa cor de background
+  })
 })
