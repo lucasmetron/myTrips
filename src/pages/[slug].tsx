12aa18f069f8709e8e6d96f85965eb92dc9767df
Lucas Rosa <lucasmetron@gmail.com>
2022-01-20 08:33:16 -0300

    configuranção codegen

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,36 +1,40 @@
 import React from 'react'
-import PageTemplate, { PageTemplateProps } from '../templates/pages/PageTemplate'
+import PageTemplate, {
+  PageTemplateProps
+} from '../templates/pages/PageTemplate'
 import client from '../graphql/client'
-import  {GET_PAGES, GET_PAGE_BY_SLUG} from '../graphql/querys'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from '../graphql/querys'
 import { useRouter } from 'next/router'
 import { GetStaticProps } from 'next'
+import { GetPageBySlugQuery, GetPagesQuery } from '../graphql/generated/graphql'
 
-export default function Page({heading, body}: PageTemplateProps) {
+export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
 
   //retorna um loading ou qualquer coisa enquanto está sendo criado
   if (router.isFallback) {
     return null
-  } 
+  }
 
   return <PageTemplate heading={heading} body={body} />
 }
 
 export async function getStaticPaths() {
-  const { pages } = await client.request(GET_PAGES, {first: 3})
-  
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
+
   const paths = pages.map(({ slug }) => ({
-    params: {slug}
+    params: { slug }
   }))
-  console.log('paths',paths)
-  return {paths, fallback: true}
+  return { paths, fallback: true }
 }
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, { slug: `${params?.slug}` })
-  
-  if(!page) return {notFound: true}
-  console.log('page',page)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
+
+  if (!page) return { notFound: true }
+
   return {
     props: {
       heading: page.heading,
@@ -38,4 +42,3 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
     }
   }
 }
-
